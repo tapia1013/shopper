b8561d8f336523f247effccf775993967a69cb3a
tapia1013 <manbearpig1013@gmail.com>
2022-05-16 09:04:07 -0700

    Footer Header styles and component imported Bootstrap and fontawesome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import { Container } from 'react-bootstrap';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 
 const App = () => {
   return (
     <>
-      <h1>Welcome To Shopper</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To Shopper</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
