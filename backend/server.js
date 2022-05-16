a9371f87b5bba24325513524f7316e3fbbac4da3
tapia1013 <manbearpig1013@gmail.com>
2022-05-16 14:44:30 -0700

    middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
@@ -13,11 +14,23 @@ connectDB();
 const app = express();
 
 
+// app.use((req, res, next) => {
+//   console.log('Hello');
+//   // next is to move to the next peice of middleware
+//   next()
+// })
+
+
 app.get('/', (req, res) => {
   res.send('Api is running...')
 })
 
 app.use('/api/products', productRoutes)
+
+// Not Found
+app.use(notFound)
+// Error middleware
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
