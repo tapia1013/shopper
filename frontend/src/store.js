fb5ccfebdaa37b14ecf9ed2fc2bae38e36a72f2a
tapia1013 <manbearpig1013@gmail.com>
2022-05-16 15:49:29 -0700

    redux state into homescreen actions reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,10 +5,13 @@ import {
 } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import {
+  productListReducer
+} from './reducers/productReducers'
 
 
 const reducer = combineReducers({
-
+  productList: productListReducer,
 })
 
 
