440489bb0cd49a95a6a67fd57ac6ecb8ce7b41b7
tapia1013 <manbearpig1013@gmail.com>
2022-05-16 18:23:16 -0700

    cart screen, actions,reducer, addtocart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,9 @@ const App = () => {
           <Routes>
             <Route exact path='/' element={<HomeScreen />} />
             <Route path='/product/:id' element={<ProductScreen />} />
-            <Route path='/cart/:id' element={<CartScreen />} />
+            <Route path='/cart' element={<CartScreen />}>
+              <Route path='/cart/:id' element={<CartScreen />} />
+            </Route>
           </Routes>
         </Container>
       </main>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -9,15 +9,26 @@ import {
   productListReducer,
   productDetailsReducer
 } from './reducers/productReducers'
+import {
+  cartReducer
+} from './reducers/cartReducers';
+
 
 
 const reducer = combineReducers({
   productList: productListReducer,
-  productDetails: productDetailsReducer
+  productDetails: productDetailsReducer,
+  cart: cartReducer
 })
 
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
-const initialState = {}
+
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromStorage
+  }
+}
 
 const middleware = [thunk]
 
