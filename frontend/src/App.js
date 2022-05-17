64fae1bd806b1e6b530d35fffeecd9cbca2739a7
tapia1013 <manbearpig1013@gmail.com>
2022-05-17 11:12:06 -0700

    user constants, reducer, action and userInfo from storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 
-
 const App = () => {
   return (
     <Router>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,21 +12,32 @@ import {
 import {
   cartReducer
 } from './reducers/cartReducers';
+import {
+  userLoginReducer
+} from './reducers/userReducers';
 
 
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  userLogin: userLoginReducer
 })
 
+// get cart items from localstorage
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+// get user info from localstorage
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 
 
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage
+  },
+  userLogin: {
+    userInfo: userInfoFromStorage
   }
 }
 
