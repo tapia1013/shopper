eed7b4dd1feb562e0760db920062d2ac584491a1
tapia1013 <manbearpig1013@gmail.com>
2022-05-17 13:06:53 -0700

    added user name to the header and implemented logout functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,21 @@
 import { LinkContainer } from 'react-router-bootstrap';
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+
+// actions
+import { logout } from '../actions/userActions';
 
 const Header = () => {
+  const dispatch = useDispatch()
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -20,16 +34,34 @@ const Header = () => {
                 </Nav.Link>
               </LinkContainer>
 
-              <LinkContainer to='/login'>
-                <Nav.Link>
-                  <i className='fas fa-user'></i> Sign In
-                </Nav.Link>
-              </LinkContainer>
+              {
+                userInfo
+                  ? (
+                    <NavDropdown title={userInfo.name} id='username'>
+                      <LinkContainer to='/profile'>
+                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                      </LinkContainer>
+
+                      <NavDropdown.Item onClick={logoutHandler}>
+                        Logout
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                  )
+                  : (
+                    <LinkContainer to='/login'>
+                      <Nav.Link>
+                        <i className='fas fa-user'></i> Sign In
+                      </Nav.Link>
+                    </LinkContainer>
+                  )
+              }
+
+
             </Nav>
           </Navbar.Collapse>
         </Container>
-      </Navbar>
-    </header>
+      </Navbar >
+    </header >
   )
 }
 
