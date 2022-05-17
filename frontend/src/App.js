2d66c3c3c7ee6a2f677fba2887b38dcea0e384e4
tapia1013 <manbearpig1013@gmail.com>
2022-05-17 12:10:52 -0700

    userlogin screen and functionality to sign in or register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const App = () => {
   return (
@@ -13,11 +14,12 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Routes>
-            <Route exact path='/' element={<HomeScreen />} />
+            <Route path='/login' element={<LoginScreen />} />
             <Route path='/product/:id' element={<ProductScreen />} />
             <Route path='/cart' element={<CartScreen />}>
               <Route path='/cart/:id' element={<CartScreen />} />
             </Route>
+            <Route exact path='/' element={<HomeScreen />} />
           </Routes>
         </Container>
       </main>
