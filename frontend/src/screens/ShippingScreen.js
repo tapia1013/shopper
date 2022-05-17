8db76b374b86dac06b96ad82a9505d8994d3c83e
tapia1013 <manbearpig1013@gmail.com>
2022-05-17 16:22:48 -0700

    checkout steps component

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
 
 // actions
 import { saveShippingAddress } from '../actions/cartActions';
@@ -31,6 +32,8 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
+
       <h1>Shipping</h1>
 
       <Form className='my-3' onSubmit={submitHandler}>
